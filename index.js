ee54651c566398bc54b4cdc5e5a6df9a680430c9
Pier2208 <p.leroy.mtl@gmail.com>
2023-06-21 13:25:55 -0400

    (build):time intensive task without bullmq

---
+++ index.js
@@ -0,0 +1,60 @@
+//https://www.digitalocean.com/community/tutorials/how-to-handle-asynchronous-tasks-with-node-js-and-bullmq
+
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const sharp = require("sharp");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+// middlewares
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(fileUpload());
+app.use(express.static("public"));
+
+// routes
+app.get("/", function (req, res) {
+  res.render("form");
+});
+
+app.get("/result", (req, res) => {
+  const imgDirPath = path.join(__dirname, "./public/images");
+  let imgFiles = fs.readdirSync(imgDirPath).map((image) => {
+    return `images/${image}`;
+  });
+  res.render("result", { imgFiles });
+});
+
+app.post("/upload", async function (req, res) {
+  const { image } = req.files;
+  if (!image) return res.sendStatus(400);
+
+  const imageName = path.parse(image.name).name; // https://www.geeksforgeeks.org/node-js-path-parse-method/
+  const processImage = (size) =>
+    sharp(image.data)
+      .resize(size, size)
+      .webp({ lossless: true })
+      .toFile(`./public/images/${imageName}-${size}.webp`);
+
+  sizes = [90, 96, 120, 144, 160, 180, 240, 288, 360, 480, 720, 1440];
+  Promise.all(sizes.map(processImage));
+
+  let counter = 0;
+  for (let i = 0; i < 10_000_000_000; i++) {
+    counter++;
+  }
+
+  res.redirect("/result");
+});
+
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
